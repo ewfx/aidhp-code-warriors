b463f2d55d08a5e6b49c21c41f9248de5e5826c6
Mounika <mounikaboorugu2312@gmail.com>
2025-03-25 12:08:22 +0530

    config updated

--- code/src/ai_recommendation_engine/src/data_loader.py
+++ code/src/ai_recommendation_engine/src/data_loader.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
 def load_customer_data():
-    profiles = pd.read_csv("./../data/customer_profiles.csv")
-    transactions = pd.read_csv("./../data/transaction_history.csv")
-    social_media = pd.read_csv("./../data/social_media_activity.csv")
-    demographics = pd.read_csv("./../data/demographic_details.csv")
-    return profiles, transactions, social_media, demographics
+    customer_profiles = pd.read_csv("code/src/ai_recommendation_engine/data/customer_profiles.csv")
+    transactions = pd.read_csv("code/src/ai_recommendation_engine/data/transaction_history.csv")
+    social_media = pd.read_csv("code/src/ai_recommendation_engine/data/social_media_activity.csv")
+    demographics = pd.read_csv("code/src/ai_recommendation_engine/data/demographic_details.csv")
+    return customer_profiles, transactions, social_media, demographics
 
 
